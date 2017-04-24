06c87894280558160fbfff36867f5cfdb717858f
Francisco Mendoza <francisco.mendoza@schibsted.cl>
2017-04-24 17:37:08 -0300

    Arreglo templates y estructura

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,32 @@
+<!-- Footer -->
+<footer class="youplay-footer-parallax">
+    <div class="wrapper" style="background-image: url('images/footer-bg.jpg')">
+
+        <!-- Social Buttons -->
+        <div class="social">
+            <div class="container">
+                <h3>Visita nuestro <strong>Facebook</strong></h3>
+
+                <div class="social-icons">
+                    <div class="social-icon">
+                        <a href="#!">
+                            <i class="fa fa-facebook-square"></i>
+                            <span>Like on Facebook</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Social Buttons -->
+
+        <!-- Copyright -->
+        <div class="copyright">
+            <div class="container">
+                &copy; 2017. Tarreo Inacap
+            </div>
+        </div>
+        <!-- /Copyright -->
+
+    </div>
+</footer>
+<!-- /Footer -->
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,212 @@
+<!-- Navbar -->
+<nav class="navbar-youplay navbar navbar-default navbar-fixed-top ">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="off-canvas" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index-2.html">
+                <img src="images/logo.png" alt="">
+            </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown dropdown-hover ">
+                    <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Inicio <span class="caret"></span> <span class="label">games</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <ul role="menu">
+                            <li><a href="store-1.html">Store Style 1</a>
+                            </li>
+                            <li><a href="store-2.html">Store Style 2</a>
+                            </li>
+                            <li><a href="cart.html">Cart</a>
+                            </li>
+                        </ul>
+                        <ul role="menu">
+                            <li><a href="store-product-1.html">Product Style 1</a>
+                            </li>
+                            <li><a href="store-product-2.html">Product Style 2</a>
+                            </li>
+                            <li><a href="checkout.html">Checkout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown dropdown-hover ">
+                    <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Inscribirme <span class="caret"></span> <span class="label">news</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <ul role="menu">
+                            <li><a href="blog-1.html">Blog Style 1</a>
+                            </li>
+                            <li><a href="blog-2.html">Blog Style 2</a>
+                            </li>
+                            <li><a href="blog-3.html">Blog Style 3</a>
+                            </li>
+                        </ul>
+                        <ul role="menu">
+                            <li><a href="blog-post-1.html">Blog Post 1</a>
+                            </li>
+                            <li><a href="blog-post-2.html">Blog Post 2</a>
+                            </li>
+                            <li><a href="blog-post-3.html">Blog Post 3</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown dropdown-hover ">
+                    <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Reglas <span class="caret"></span> <span class="label">full list</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <ul role="menu">
+                            <li class="dropdown dropdown-submenu pull-left ">
+                                <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">User</a>
+                                <div class="dropdown-menu">
+                                    <ul role="menu">
+                                        <li><a href="user-activity.html">Activity</a>
+                                        </li>
+                                        <li><a href="user-profile.html">Profile</a>
+                                        </li>
+                                        <li><a href="user-messages.html">Messages</a>
+                                        </li>
+                                        <li><a href="user-messages-compose.html">Messages Compose</a>
+                                        </li>
+                                        <li><a href="user-settings.html">Settings</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li><a href="forums.html">Forums</a>
+                            </li>
+                            <li><a href="forums-topics.html">Forums Topics</a>
+                            </li>
+                            <li><a href="forums-single-topic.html">Single Topic</a>
+                            </li>
+                            <li><a href="matches-list.html">Matches List</a>
+                            </li>
+                            <li><a href="match.html">Match</a>
+                            </li>
+                            <li><a href="match-2.html">Match with Maps</a>
+                            </li>
+                        </ul>
+                        <ul role="menu">
+                            <li><a href="widgets.html">Widgets <span class="badge bg-default">New</span></a>
+                            </li>
+                            <li><a href="components.html">Components</a>
+                            </li>
+                            <li><a href="coming-soon.html">Coming Soon</a>
+                            </li>
+                            <li><a href="contact.html">Contact Us</a>
+                            </li>
+                            <li><a href="search.html">Search</a>
+                            </li>
+                            <li><a href="login.html">Login</a>
+                            </li>
+                            <li><a href="404.html">404</a>
+                            </li>
+                            <li><a href="blank.html">Blank</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown dropdown-hover">
+                    <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        John Doe <span class="badge bg-default">2</span> <span class="caret"></span> <span class="label">it is you</span>
+                    </a>
+                    <div class="dropdown-menu">
+                        <ul role="menu">
+                            <li><a href="http://html.nkdev.info/youplay/documentation">Documentation</a>
+                            </li>
+                            <li><a href="http://themeforest.net/item/youplay-game-template-based-on-bootstrap/11306207?ref=_nK">Purchase</a>
+                            </li>
+                            <li class="divider"></li>
+
+                            <li><a href="user-profile.html">Profile <span class="badge pull-right bg-warning">13</span></a>
+                            </li>
+                            <li><a href="cart.html">My Cart <span class="badge pull-right bg-default">3</span></a>
+                            </li>
+                            <li class="divider"></li>
+
+                            <li><a href="login.html">Log Out</a>
+                            </li>
+                            <li class="dropdown dropdown-submenu pull-left">
+                                <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">More..</a>
+                                <div class="dropdown-menu">
+                                    <ul role="menu">
+                                        <li><a href="#!">3rd level</a>
+                                        </li>
+                                        <li><a href="#!">3rd level</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a class="search-toggle" href="search.html">
+                        <i class="fa fa-search"></i>
+                    </a>
+                </li>
+                <li class="dropdown dropdown-hover dropdown-cart">
+                    <a href="#!" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        <i class="fa fa-shopping-cart"></i>
+                    </a>
+                    <div class="dropdown-menu" style="width: 300px;">
+                        <div class="row youplay-side-news">
+                            <div class="col-xs-3 col-md-4">
+                                <a href="#" class="angled-img">
+                                    <div class="img">
+
+                                        <img src="images/game-bloodborne-500x375.jpg" alt="">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xs-9 col-md-8">
+                                <a href="#" style="text-decoration: none;" class="pull-right mr-10"><i class="fa fa-times"></i></a>
+
+                                <h4 class="ellipsis"><a href="#">Bloodborne</a></h4>
+                                <span class="quantity">1 × <span class="amount">$50.00</span></span>
+                            </div>
+                        </div>
+
+                        <div class="row youplay-side-news">
+                            <div class="col-xs-3 col-md-4">
+                                <a href="#" class="angled-img">
+                                    <div class="img">
+
+                                        <img src="images/game-kingdoms-of-amalur-reckoning-500x375.jpg" alt="">
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-xs-9 col-md-8">
+                                <a href="#" style="text-decoration: none;" class="pull-right mr-10"><i class="fa fa-times"></i></a>
+
+                                <h4 class="ellipsis"><a href="#">Kingdoms of Amalur</a></h4>
+                                <span class="quantity">1 × <span class="amount">$20.00</span></span>
+                            </div>
+                        </div>
+
+                        <div class="ml-20 mr-20 pull-right"><strong>Subtotal:</strong>  <span class="amount">$70.00</span>
+                        </div>
+
+                        <div class="btn-group pull-right m-15">
+                            <a href="#" class="btn btn-default btn-sm">View Cart</a>
+                            <a href="#" class="btn btn-default btn-sm">Checkout</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- /Navbar -->
